e57a52393384948977558fe055049d8a2cf74a3f
rlitoncs <rj_liton@hotmail.com>
2024-05-25 22:16:45 -0400

    Redirect user to original link provided from long URL

--- express_server.js
+++ express_server.js
@@ -36,6 +36,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
