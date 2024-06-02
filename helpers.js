a8e2e1989d2804d273bc605a86b701a004e5c63e
rlitoncs <rj_liton@hotmail.com>
2024-06-01 23:12:38 -0400

    Add comments and description for helper functions

--- helpers.js
+++ helpers.js
@@ -1,5 +1,24 @@
+/**
+ * generateRandomString() function creates a random 6 alpha-numeric character ID for a short URL
+ * 
+ * findUserByEmail(email, database) function handles incoming user emails and identifies whether emails are found in the database. 
+ * @param { String } email - the user email
+ * @param { Object } database - the users database
+ * @return { Object || null } - returns the userID object if found or otherwise null
+ * 
+ * urlsForUser(id) functions returns all current URLS for the given user ID
+ * @param { String } id - the current user ID
+ */
+
+//==============================================================================
+// Dependencies
+//==============================================================================
 const { urlDatabase } = require('./CONSTANTS');
 
+
+//==============================================================================
+// Helper Functions
+//==============================================================================
 const generateRandomString = () => {
   const random_short_url = Math.random().toString(36).slice(2, 8);
   return random_short_url;
@@ -7,12 +26,10 @@ const generateRandomString = () => {
 
 const findUserByEmail = (email, database) => {
   for (let userID in database) {
-    // return userID if found
     if (database[userID].email === email) {
       return userID;
     }
   }
-  //return null if no user is found
   return null;
 };
 
@@ -24,8 +41,11 @@ const urlsForUser = (id) => {
       userURLS[shortID] = urlDatabase[shortID];
     }
   }
-  
+
   return userURLS;
 };
 
+//==============================================================================
+// Module Exports
+//==============================================================================
 module.exports = { generateRandomString, findUserByEmail, urlsForUser };
