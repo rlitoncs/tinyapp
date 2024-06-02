7fb773dc6d76f582ff1412e666ffb1b7ae2ec3ad
rlitoncs <rj_liton@hotmail.com>
2024-06-01 22:57:09 -0400

    ESLINT express_server.js and helpers.js

--- express_server.js
+++ express_server.js
@@ -39,8 +39,8 @@ app.use(cookieSession({
 
 //GET /
 app.get("/", (req, res) => {
-  return res.redirect("/login")
-})
+  return res.redirect("/login");
+});
 
 //GET /urls
 app.get("/urls", (req, res) => {
@@ -49,27 +49,27 @@ app.get("/urls", (req, res) => {
   const userID = req.session.user_id;
 
   //error handling: user is not logged in
-  if (!userID){
-    return res.status(401).send("401 Unauthorized. Please Login or Register")
+  if (!userID) {
+    return res.status(401).send("401 Unauthorized. Please Login or Register");
   }
 
   const getUserURLS = urlsForUser(userID);
-  const templateVars = { 
+  const templateVars = {
     urls: getUserURLS,
     user: users[userID],
-   };
+  };
 
   res.render("urls_index", templateVars);
 
 });
 
 // POST /urls
-app.post("/urls", (req, res) => { 
+app.post("/urls", (req, res) => {
 
   const userID = req.session.user_id;
 
   //error handling: users cannot shorten URLs if not logged in
-  if(!userID){
+  if (!userID) {
     return res.status(404).send("You must login to shorten URLs");
   }
   
@@ -90,16 +90,16 @@ app.get("/urls/new", (req, res) => {
   const userID = req.session.user_id;
 
   //error handling: redirect users who are not logged in
-  if(!userID){
-    return res.redirect("/login")
+  if (!userID) {
+    return res.redirect("/login");
   }
 
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
     user: users[userID],
   };
 
-  res.render("urls_new", templateVars);  
+  res.render("urls_new", templateVars);
 
 });
 
@@ -108,23 +108,23 @@ app.get("/urls/:id", (req, res) => {
 
   const userID = req.session.user_id;
   const getUserURLS = urlsForUser(userID);
-  const short_url_id = req.params.id
+  const short_url_id = req.params.id;
 
   // error handling: users cannot access short urls if not logged in
-  if (!userID){
-    return res.status(404).send("404 Not Found. Please Login to access URLS")
+  if (!userID) {
+    return res.status(404).send("404 Not Found. Please Login to access URLS");
   }
 
   // error handling: users can only access URLS they own/created
-  if (!(getUserURLS[short_url_id])){
+  if (!(getUserURLS[short_url_id])) {
     return res.status(403).send("403 Forbidden. Requested URL does not exist. You are trying to access a URL you do not yet own");
   }
 
-  const templateVars = { 
-    id: short_url_id, 
+  const templateVars = {
+    id: short_url_id,
     longURL: getUserURLS[short_url_id].longURL,
     user: users[userID],
-   };
+  };
 
   res.render("urls_show", templateVars);
 
@@ -136,16 +136,16 @@ app.post("/urls/:id", (req,res) => {
   
   const userID = req.session.user_id;
   const getUserURLS = urlsForUser(userID);
-  const short_url_id = req.params.id
+  const short_url_id = req.params.id;
 
-  //error handling: users not logged in cannot edit short URLs 
-  if (!userID){
-    return res.status(401).send("401 Unauthorized to edit. Please Login")
+  //error handling: users not logged in cannot edit short URLs
+  if (!userID) {
+    return res.status(401).send("401 Unauthorized to edit. Please Login");
   }
   
   //error handloing: logged in users cannot edit a short URL it does not own
-  if (!(getUserURLS[short_url_id])){
-    return res.status(404).send(`404 Not Found. ${short_url_id} does not exist `)
+  if (!(getUserURLS[short_url_id])) {
+    return res.status(404).send(`404 Not Found. ${short_url_id} does not exist `);
   }
 
   getUserURLS[short_url_id].longURL = req.body.longURL;
@@ -156,11 +156,11 @@ app.post("/urls/:id", (req,res) => {
 
 //GET /u/:id
 app.get("/u/:id", (req, res) => {
-  const short_url_id = req.params.id
+  const short_url_id = req.params.id;
   
-   //error Handling: short URLs must exist in the database
-   if (!urlDatabase[short_url_id]){
-    return res.status(404).send("404 Not Found. Short URL does not exist")
+  //error Handling: short URLs must exist in the database
+  if (!urlDatabase[short_url_id]) {
+    return res.status(404).send("404 Not Found. Short URL does not exist");
   }
 
   const longURL = urlDatabase[short_url_id].longURL;
@@ -176,14 +176,14 @@ app.post("/urls/:id/delete", (req, res) => {
   const getUserURLS = urlsForUser(userID);
   const short_url_id = req.params.id;
 
-  //error handling: users not logged in cannot delete short URLs 
-  if (!userID){
-    return res.status(401).send("401 Unauthorized to delete. Please Login")
+  //error handling: users not logged in cannot delete short URLs
+  if (!userID) {
+    return res.status(401).send("401 Unauthorized to delete. Please Login");
   }
 
   //error handling: users logged in cannot delete short URLS they do not own
-  if (!(getUserURLS[short_url_id])){
-    return res.status(404).send(`404 Not Found. ${short_url_id} does not exist `)
+  if (!(getUserURLS[short_url_id])) {
+    return res.status(404).send(`404 Not Found. ${short_url_id} does not exist `);
   }
 
   delete urlDatabase[short_url_id];
@@ -196,45 +196,45 @@ app.get('/login', (req,res) => {
 
   const userID = req.session.user_id;
   
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
     user: users[userID],
   };
   
   if (userID) {
-    res.redirect("/urls")
+    res.redirect("/urls");
   } else {
     res.render("login", templateVars);
   }
-})
+});
 
 // POST /login
 app.post("/login", (req,res) => {
   const userEmail = req.body.email;
   const userPassword = req.body.password;
-  const userID = findUserByEmail(userEmail, users); 
+  const userID = findUserByEmail(userEmail, users);
 
   //error handling: empty email or password
-  if (!userEmail || !userPassword){
+  if (!userEmail || !userPassword) {
     return res.status(404).send('404 Not Found. Please provide a valid email address and password');
   }
 
   //error handling: email non-existent or password mismatch
-  if (!users[userID]){
+  if (!users[userID]) {
     return res.status(403).send('403 Forbidden. Email does not exist');
-  } else if (!bcrypt.compareSync(userPassword, users[userID].password)){
+  } else if (!bcrypt.compareSync(userPassword, users[userID].password)) {
     return res.status(403).send('403 Forbidden. Incorrect Password.');
   }
 
   req.session.user_id = userID;
   res.redirect("/urls");
-})
+});
 
 // POST /logout
 app.post("/logout", (req,res) => {
   req.session = null;
   res.redirect("/login");
-})
+});
 
 
 // GET /register
@@ -242,18 +242,18 @@ app.get("/register", (req,res) => {
 
   const userID = req.session.user_id;
 
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
     user: users[userID],
   };
 
   if (userID) {
-    res.redirect("/urls")
+    res.redirect("/urls");
   } else {
     res.render("register", templateVars);
   }
 
-})
+});
 
 // POST /register
 app.post("/register", (req, res) => {
@@ -262,12 +262,12 @@ app.post("/register", (req, res) => {
   const hashedPassword = bcrypt.hashSync(userPassword, 10);
  
   //error handling: empty email or password
-  if (!userEmail || !userPassword){
+  if (!userEmail || !userPassword) {
     return res.status(400).send('400 Bad Request. Please provide a valid email and password');
   }
   //error handling: email exists
   const user = findUserByEmail(userEmail, users);
-  if (user){
+  if (user) {
     return res.status(400).send('400 Bad Request. Email has already been registered');
   }
 
@@ -275,14 +275,14 @@ app.post("/register", (req, res) => {
   const userID = generateRandomString();
   //add user
   users[userID] = {
-    id: userID, 
-    email: userEmail, 
+    id: userID,
+    email: userEmail,
     password: hashedPassword
   };
   
   req.session.user_id = userID;
   res.redirect("/urls");
-})
+});
 
 //==============================================================================
 // Listener
--- helpers.js
+++ helpers.js
@@ -3,32 +3,30 @@ const { urlDatabase } = require('./CONSTANTS');
 const generateRandomString = () => {
   const random_short_url = Math.random().toString(36).slice(2, 8);
   return random_short_url;
-}
+};
 
 const findUserByEmail = (email, database) => {
   for (let userID in database) {
     // return userID if found
-    if (database[userID].email === email){
+    if (database[userID].email === email) {
       return userID;
     }
   }
   //return null if no user is found
   return null;
-}
+};
 
 const urlsForUser = (id) => {
   const userURLS = {};
 
-  for (let shortID in urlDatabase){
-    if (urlDatabase[shortID].userID === id){
+  for (let shortID in urlDatabase) {
+    if (urlDatabase[shortID].userID === id) {
       userURLS[shortID] = urlDatabase[shortID];
     }
   }
 
   console.log(userURLS);
   return userURLS;
-}
-
-
+};
 
 module.exports = { generateRandomString, findUserByEmail, urlsForUser };
