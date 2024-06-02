f0d4efa60f6b4c57bc3f688ce30c2e883f4b98c1
rlitoncs <rj_liton@hotmail.com>
2024-06-01 22:56:45 -0400

    Refactor comments and ESLINT code

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -12,12 +12,12 @@ describe("Login and Access Control Test", () => {
 
     return agent
       .get("/")
-      .redirects(0) 
+      .redirects(0)
       .then((res) => {
         expect(res).to.have.status(302);
-      })
+      });
 
-      //without the .redirects(0), res will show the final response which is status 200. With .redirects(0), it instructs Chai HTTP not to automatically follow any redirects. Tells Chai HTTP to stop the request and respond with the redirect response instead of automatically following the redirect.
+    //without the .redirects(0), res will show the final response which is status 200. With .redirects(0), it instructs Chai HTTP not to automatically follow any redirects. Tells Chai HTTP to stop the request and respond with the redirect response instead of automatically following the redirect.
   });
 
   
@@ -52,48 +52,39 @@ describe("Login and Access Control Test", () => {
       .post("/login")
       .send({ email: "a@example.com", password: "123" })
       .then((loginRes) => {
-        // Step 2: Make a GET request to a protected resource
         return agent.get("/urls/abc").then((accessRes) => {
-          // Step 3: Expect the status code to be 403
           expect(accessRes).to.have.status(403);
         });
       });
   });
 
 
-
+  // 5. GET /urls/:id, a user should see an error message if they do not own the URL
   it('should return 403 status code for unauthorized access to "http://localhost:8000/urls/b2xVn2"', () => {
     const agent = chai.request.agent("http://localhost:8000");
 
-    // 5. GET /urls/:id, a user should see an error message if they do not own the URL
     return agent
       .post("/login")
       .send({ email: "user2@example.com", password: "dishwasher-funk" })
       .then((loginRes) => {
-        // Step 2: Make a GET request to a protected resource
         return agent.get("/urls/b2xVn2").then((accessRes) => {
-          // Step 3: Expect the status code to be 403
           expect(accessRes).to.have.status(403);
         });
       });
   });
 
+  // POST /urls/:id, a logged in user should see an error message if they do not own the URL
   it('should return 404 status code when user is logged in and tries to edit a URL it does not own, "http://localhost:8000/urls/abc"', () => {
     const agent = chai.request.agent("http://localhost:8000");
 
-    // POST /urls/:id, a logged in user should see an error message if they do not own the URL
     return agent
       .post("/login")
       .send({ email: "user2@example.com", password: "dishwasher-funk" })
       .then((loginRes) => {
-        // Step 2: Make a POST request to a protected resource
         return agent.post("/urls/abc").then((accessRes) => {
-          // Step 3: Expect the status code to be 404
           expect(accessRes).to.have.status(404);
         });
       });
   });
-
-
 
 });
